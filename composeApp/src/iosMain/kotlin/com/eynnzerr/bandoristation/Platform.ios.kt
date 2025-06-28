e1bd0a74f4e1dfd6c5b6ea2bb9c85a44e6781528
eynnzerr <809204291@qq.com>
2025-06-28 16:16:57 +0800

    fix: change fallback versionName

--- composeApp/src/iosMain/kotlin/com/eynnzerr/bandoristation/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/eynnzerr/bandoristation/Platform.ios.kt
@@ -5,7 +5,7 @@ import platform.Foundation.NSBundle
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    override val versionName: String = NSBundle.mainBundle.infoDictionary?.get("CFBundleShortVersionString") as? String ?: "1.0.0-Unknown"
+    override val versionName: String = NSBundle.mainBundle.infoDictionary?.get("CFBundleShortVersionString") as? String ?: "1.0.0"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
